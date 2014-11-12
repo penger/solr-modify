bcb4d57768391f9eff6cab74b0329d7c2ee1c512
gongpeng <gongpeng@home.com>
2014-11-12 11:14:54 +0800

    add some comments on the nlptest

--- src/main/java/testnlp/WordAlertTest.java
+++ src/main/java/testnlp/WordAlertTest.java
@@ -1,6 +1,8 @@
 package testnlp;
 
+
 import java.io.BufferedReader;
+
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -16,8 +18,16 @@ import org.nlpcn.commons.lang.pinyin.PinyinWord;
 import org.nlpcn.commons.lang.standardization.WordUtil;
 import org.nlpcn.commons.lang.util.WordAlert;
 
+/**
+ * nlp 的测试
+ * https://github.com/NLPchina/nlp-lang
+ * @author Administrator
+ *
+ */
+
 public class WordAlertTest {
 
+	//隔离乱码
 	   @Test
 	    public void test() {
 	        String str = "ａｚ ＡＺ AZ az ０９•" ;
@@ -41,6 +51,7 @@ public class WordAlertTest {
 	        Assert.assertEquals(parseStr.size(), str.length());
 	    }
 	    
+	    //特征显示
 	    @Test
 	    public void hh(){
 	    	String content = "卓尔防线继续伤筋动骨 队长梅方出场再补漏说起来卓尔队长梅方本赛季就是个“补漏”的命！在中卫与右边后卫间不停地轮换。如果不出意外，今天与广州恒大一战梅方又要换位置，这也是汉军队长连续三场比赛中的第三次换位。而从梅方的身上也可以看出，本赛季汉军防线如此“折腾”，丢球多也不奇怪了。梅方自2009赛季中乙出道便一直司职中后卫，还曾入选过布拉泽维奇国奥队，也是司职的中卫。上赛季，梅方与忻峰搭档双中卫帮助武汉卓尔队中超成功，但谁知进入本赛季后从第一场比赛开始梅方便不断因为种种“意外”而居无定所。联赛首战江苏舜天时，也是由于登贝莱受伤，朱挺位置前移，梅方临危受命客串右边后卫。第二轮主场与北京国安之战梅方仅仅打了一场中卫，又因为柯钊受罚停赛4轮而不得不再次到边路“补漏”。随着马丁诺维奇被弃用，梅方一度成为中卫首选，在与上海东亚队比赛中，邱添一停赛，梅方与忻峰再度携手，紧接着与申鑫队比赛中移至边路，本轮忻峰又停赛，梅方和邱添一成为中卫线上最后的选择。至于左右边后卫位置，卓尔队方面人选较多，罗毅、周恒、刘尚坤等人均可出战。记者马万勇原标题：卓尔防线继续伤筋动骨队长梅方出场再补漏稿源：中新网作者：";
@@ -50,6 +61,7 @@ public class WordAlertTest {
 	        System.out.println(new FingerprintService().fingerprint(content2));
 	    }
 	    
+	    //search suggestion
 	    @Test
 	    public void hh2(){
 	    	   //构建一个内存型索引
@@ -72,6 +84,8 @@ public class WordAlertTest {
 	        System.out.println(mi.smartSuggest("中过"));
 	    }
 	    
+	    
+	    //boolmfilter
 	    @Test
 	    public  void jj() throws Exception {
 	    	
