2c03cf5ab0b516e13615e7bb682052e1be7d1055
penger <gongpengllpp@sina.com>
2014-11-17 15:41:12 +0800

    add quick sort code

--- src/main/java/cn/archangel/sort/ShellSort.java
+++ src/main/java/cn/archangel/sort/ShellSort.java
@@ -8,9 +8,60 @@ package cn.archangel.sort;
 public class ShellSort {
 
 	public static void main(String[] args) {
-		Integer a[] = new Integer[]{3,1,5,7,2,4,9,6};
-		shellSort(a, 8);
+		Integer a[] = new Integer[]{4,3,5,8,9,2,7,1};
+//		shellSort(a, 8);
+		for (Integer temp : a) {
+			System.out.print(temp);
+		}
+		System.out.println();
+		quickSort(a, 0, a.length-1);
+		for (Integer temp : a) {
+			System.out.print(temp);
+		}
+		System.out.println();
 	}
+	
+	
+	/**
+	 * 快速排序
+	 * @param a
+	 * @param start
+	 * @param end
+	 */
+	private static void quickSort(Integer[] a,int start,int end) 
+	   { 
+		System.out.println("start is:"+start+"  end is :"+end);
+	       if(start<end) 
+	       {
+	           int key=a[start];//初始化保存基元  
+	           int i=start,j;//初始化i,j  
+	           for(j=start+1;j<=end;j++) {
+	             System.out.println("j="+j+"   a[j]="+a[j]);
+	               if(a[j]<key)//如果此处元素小于基元，则把此元素和i+1处元素交换，并将i加1，如大于或等于基元则继续循环  
+	               {
+	            	   
+	            	   System.out.println("调换数据"+(i+1)+" 和"+j+"值为:"+a[i+1]+"和"+a[j]);
+	            	   int temp=a[j]; 
+	                   a[j]=a[i+1]; 
+	                   a[i+1]=temp; 
+	                   i++; 
+	                   for (Integer item : a) {
+	   	   					System.out.print(item);
+		   	   			}
+		   	           System.out.println("-----");
+		               } 
+	           }
+	           
+	           a[start]=a[i];//交换i处元素和基元  
+	           a[i]=key;
+	           for (Integer temp : a) {
+	   				System.out.print(temp);
+	   			}
+	           System.out.println();
+	           quickSort(a, start, i-1);//递归调用 
+	           quickSort(a, i+1, end);
+	       }
+	   }
 	
 	
 	private static void shellSort(Integer[] a,int n){
@@ -24,7 +75,7 @@ public class ShellSort {
 
 	private static void shellInsertSort(Integer[] a, int n, int dk) {
 		for(int i=dk;i<n;++i){
-			System.out.println("i"+i+"i-dk"+i+"-"+dk+"="+(i-dk));
+			System.out.println("i  "+i+"   i-dk"+i+"-"+dk+"  ="+(i-dk));
 			if(a[i]<a[i-dk]){
 				int j=i-dk;
 				System.out.println("j"+j);
